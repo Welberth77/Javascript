bded18cef3a3ec0e7126cf0b6cd83aa687e3b793
Welberth <welbertholiveira250@gmail.com>
2025-03-20 19:48:39 -0300

    modificando nomes dos arquivos para aulas

---
+++ aula05.js
@@ -0,0 +1,21 @@
+// Objetos
+const nome = "Pedro";
+const sobrenome = "Silva";
+const idade = 20;
+const estado = "SP";
+const passatempo = ["Música", "Filmes", "VideoGame", "Futebol"];
+
+// Definindo objeto
+const pessoa = {
+    nome: "Pedro",
+    sobrenome: "Silva",
+    idade: 20,
+    estado: "SP",
+    passatempo: ["Música", "Filmes", "VideoGame", "Futebol"]
+} 
+
+// Imprimindo todos os dados    
+console.log(pessoa);
+
+// Imprimindo apenas o nome
+console.log(pessoa.nome)
