18dce63f8f6f82db6faea7596e84d1aa1150ab4d
Welberth <welbertholiveira250@gmail.com>
2025-03-21 14:32:41 -0300

    manipulação nos objetos em javascript

--- aula05.js
+++ aula05.js
@@ -1,10 +1,4 @@
 // Objetos
-const nome = "Pedro";
-const sobrenome = "Silva";
-const idade = 20;
-const estado = "SP";
-const passatempo = ["Música", "Filmes", "VideoGame", "Futebol"];
-
 // Definindo objeto
 const pessoa = {
     nome: "Pedro",
@@ -12,10 +6,35 @@ const pessoa = {
     idade: 20,
     estado: "SP",
     passatempo: ["Música", "Filmes", "VideoGame", "Futebol"]
-} 
+};
 
 // Imprimindo todos os dados    
 console.log(pessoa);
 
 // Imprimindo apenas o nome
-console.log(pessoa.nome)
+console.log(pessoa.nome);
+
+// -----------------------
+console.log("-----------------------");
+
+let tarefas = [
+    {
+        id: 1,
+        texto: "Tirar lixo",
+        completo: false
+    },
+    {
+        id: 2,
+        texto: "Varrer chão",
+        completo: false
+    }
+];
+
+// Alterando valor de um vetor de objetos
+tarefas[1].completo = true;
+
+console.log(tarefas);
+
+// Tranformando objetos no modo json de dados
+
+console.log(JSON.stringify(tarefas));
